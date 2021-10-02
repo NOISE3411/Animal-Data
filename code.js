908f6c09b0b63d71d5b9c2a90ef2efbb707962db
Anand <87514488+httpanand@users.noreply.github.com>
2021-10-02 21:35:32 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,39 @@
+onEvent("gotoselectionscreen", "click", function( ) {
+  setScreen("selectionscreen");
+});
+onEvent("LION", "click", function( ) {
+  setScreen("LIONSCREEN");
+});
+onEvent("TIGER", "click", function( ) {
+  setScreen("TIGERSCREEN");
+});
+onEvent("CHEETAH", "click", function( ) {
+  setScreen("CHEETAHSCREEN");
+});
+onEvent("ELEPHANT", "click", function( ) {
+  setScreen("ELEPHANTSCREEN");
+});
+onEvent("GIRAFFE", "click", function( ) {
+  setScreen("GIRAFFESCREEN");
+});
+onEvent("BLACKPANTHER", "click", function( ) {
+  setScreen("BLACKPANTHERSCREEN");
+});
+onEvent("BACK1", "click", function( ) {
+  setScreen("selectionscreen");
+});
+onEvent("BACK2", "click", function( ) {
+  setScreen("selectionscreen");
+});
+onEvent("BACK3", "click", function( ) {
+  setScreen("selectionscreen");
+});
+onEvent("BACK4", "click", function( ) {
+  setScreen("selectionscreen");
+});
+onEvent("BACK5", "click", function( ) {
+  setScreen("selectionscreen");
+});
+onEvent("BACK6", "click", function( ) {
+  setScreen("selectionscreen");
+});
